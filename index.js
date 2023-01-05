8c2b42a3031f5c99cd34d3f8139b5184b25ef26f
Kayla Volk <kaylavolk8@gmail.com>
2023-01-04 20:28:50 -0500

    commit tree e8b5b3339258c08bc97d479732180dc4a86c4c1e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData(firstName, email){
+    const configurationObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": firstName,
+            "email": email
+        })
+};
+return fetch("http://localhost:3000/users", configurationObj)
+.then(res => res.json())
+.then(newUser => document.body.innerHTML = newUser['id'])
+
+.catch(error => document.body.innerHTML = error.message)
+
+}
